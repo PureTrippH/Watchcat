4b1ec3aad82aeb1bbb54f5dc360ceb6f9fbe67ff
Tripp H <capehenrykid@gmail.com>
2020-08-11 20:14:49 -0400

    Mute command

---
+++ mute.js
@@ -0,0 +1,40 @@
+exports.run = async (client, message, args) => {
+    const serverSettings = require("../data/serversettings.json");
+    const channel = (!serverSettings[message.guild.id]) ? null : serverSettings[message.guild.id].channel;
+    const role = (!serverSettings[message.guild.id]) ? null : serverSettings[message.guild.id].role;
+    const fs = require("fs");
+
+    if(message.author.id == "700214206808719432") {
+    message.delete({ timeout: 200 });
+        message.author.send({embed: {
+            color: 0xff0000,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: `Laela's Watchdog's`,
+            timestamp: new Date(),
+            fields: [
+                {
+                    name: 'Get Stickbugged lol',
+                    value: 'https://www.youtube.com/watch?v=gPPKHt6hFCA'
+                }
+            ],
+            image: {
+              url: 'https://media.tenor.com/images/36dd82e085114a98fe9cfe428d7a4031/tenor.gif',
+            },
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: client.user.username
+            },
+          }
+        });
+        return;
+};
+}
+
+module.exports.help = {
+	name: "Mute",
+	desc: "Mutes a User for 10 minutes",
+	usage: "l^mute (tagged user) [optional reason]"
+}
