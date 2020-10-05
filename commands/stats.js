c09026ec9e952afe6b538b187d9e9ffadd33398c
Tripp2003171! <capehenrykid@gmail.com>
2020-10-04 20:41:47 -0400

    Beginning of Rank

--- commands/stats.js
+++ commands/stats.js
@@ -9,25 +9,41 @@ exports.run = async (client, message, args) => {
 	
 	
 
-	const levelBoard = Canvas.createCanvas(500, 200);
+	const levelBoard = Canvas.createCanvas(500, 200, {
+        legend: {
+			itemMaxWidth: 150,
+			itemWrap: true
+        }});
 	
 	const ctx = levelBoard.getContext("2d");
-	const background = await Canvas.loadImage("https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+	const background = await Canvas.loadImage("https://img.wallpapersafari.com/desktop/1536/864/93/84/x7tXzR.jpg");
+	
+	ctx.font = "20px Arial";
+	ctx.fillText("Test", 4, 4);
 	ctx.drawImage(background, 0, 0, levelBoard.width, levelBoard.height);
-	ctx.fillStyle = "rgba(57, 61, 72, 0.6)";
+	ctx.fillStyle = "rgba(57, 61, 72, 0.9)";
 	ctx.fillRect(0,0,levelBoard.width, 50);
-	ctx.quadraticCurveTo(0,0,255,2);
+	ctx.font = "40px Arial";
+	ctx.fillStyle = "white";
+	ctx.fillText(message.member.displayName, 15, 40);
+	ctx.fillStyle = "yellow";
+	(message.author.id == "168695206575734784") ? ctx.fillText("🏵️", 440, 37) : ctx.fillText("", 450, 30);
+	
+	const user = await queries.queryUser(message.guild.id, message.author.id);
+	console.log(user.guildMembers[0].messageCount);
+	
+
+
 	const exportImage = new Discord.MessageAttachment(levelBoard.toBuffer(), "levelBoard.png");
+	
     embed.setColor('#e3bcf7');
     
     embed.setFooter('Credit to CEO of Racism#2051 for Level Equation');
     embed.setTimestamp();
 	embed.attachFiles(exportImage);
 	
-    ctx.font="50px Arial";
-	ctx.lineWidth = 1;
-	ctx.strokeStyle= "black"
-    ctx.strokeText("Test", 0, 0);
+	
+    
 
 	message.channel.send(embed);
 }
