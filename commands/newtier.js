10fc267fd42dc72aaa540dbda5063028370aa270
Tripp H <capehenrykid@gmail.com>
2020-11-22 16:30:18 -0500

    NewTier work

--- commands/newtier.js
+++ commands/newtier.js
@@ -1,10 +1,15 @@
 exports.run = async (client, message, args) => {
 //Defined Required Modules and Packages.
+	const Discord = require("discord.js");
+	const embedTemp = new Discord.MessageEmbed();
 	const ms = require("ms");
+	
 	const redis = require('../utils/redis');
 	const serverStats = require("../utils/schemas/serverstat.js");
 	const serverConfig = require("../utils/schemas/serverconfig.js");
 	const queries = require("../utils/queries/queries.js");
+
+	if(!args.length || !args[1]) return message.channel.send("No User Was Mentioned for the tiering");
 
 	const tierArg = args[1].toLowerCase();
 	const reason = args.slice(2).join(" ") || "Unknown Reason";
@@ -13,14 +18,8 @@ exports.run = async (client, message, args) => {
 
 	const dbResConfig = await queries.queryServerConfig(message.guild.id);
 	const user = await queries.queryUser(message.guild.id, tagged.id);
-	
-	const date = new Date();
-	const dd = String(date.getDate()).padStart(2, '0');
-	const mm = String(date.getMonth() + 1).padStart(2, '0'); 
-	const yyyy = date.getFullYear();
-	const banDate = { dd: dd, mm: mm, yyyy: yyyy}
-	const tierIndex = dbResConfig.serverTiers.findIndex(tier => tier.TierName === tierArg);
 
+	const tierAction = require('../utils/queries/tierUtils/tierUtils.js');
 
 	const dependencies = {
 		redis,
@@ -30,9 +29,8 @@ exports.run = async (client, message, args) => {
 
 	//Checks for Permissions and Args validity
 	if(message.member.hasPermission('BAN_MEMBERS') || message.author.id == '168695206575734784') {
-	if(!tagged || !args.length) return message.channel.send("No User Was Mentioned for the tiering");
 
-	const user = await serverConfig.findOne(
+	const tier = await serverConfig.findOne(
 		{
 		  guildId: message.guild.id,
 		}, 
@@ -45,10 +43,98 @@ exports.run = async (client, message, args) => {
 		  }
 		});
 
-		console.log(user);
+		if((tier.serverTiers).length == 0) return message.channel.send("No Tier Found");
+		let punishArray = user.guildMembers[0].punishmentsTiers;
+		let userRoles = tagged._roles;
+		
+
+		const lastTier = (typeof punishArray[punishArray.findIndex(tierObj => tierObj.tierName === tierArg)] === 'undefined')? 0 : punishArray[punishArray.findIndex(tierObj => tierObj.tierName === tierArg)].tierLevel;
+		
+
+		let redisClient = await redis();
+
+		tierAction.addTier(client, message, user, tier, userRoles, tierArg, lastTier, tagged);
+		const tierType = await tierAction.matchPunishment(tier, dbResConfig, lastTier);
+		console.log(tierType);
+			switch(tierType) {
+				case "warning":
+					await awaitWarn(tierAction, embedTemp, tier, tagged, reason, tierArg, dbResConfig, lastTier, redisClient, ms, message);
+				break;
+				case "ban":
+					await awaitBan(tierAction, embedTemp, tier, tagged, reason, tierArg, dbResConfig, lastTier, redisClient, ms, message);
+				break;
+
+				case "mute":
+					await awaitMute(tierAction, embedTemp, tier, tagged, reason, tierArg, dbResConfig, lastTier, redisClient, ms, message);
+				break;
+			}
+			redisClient.quit();
 
 	}
 };
+
+
+const awaitWarn = async(tierAction, embedTemp, tier, tagged, reason, tierArg, dbResConfig, lastTier, redisClient, ms, message) => {
+	console.log("warn");
+	embedTemp.setTitle("You Have Been Tiered!");
+	embedTemp.setColor("#ff0000");
+	embedTemp.addFields({ name: `Tier:`, value: `${tierArg}`, inline: false });
+	embedTemp.addFields({ name: `Type:`, value: `${tierAction.matchPunishment(tier, dbResConfig, lastTier)}`, inline: false });
+	embedTemp.addFields({ name: `Time:`, value: `${ms(await tierAction.matchTier(tier, dbResConfig, lastTier))}`, inline: false });
+	embedTemp.addFields({ name: `Reason:`, value: `${reason}`, inline: false });
+	tagged.send(embedTemp);
+}
+
+const  awaitMute = async(tierAction, embedTemp, tier, tagged, reason, tierArg, dbResConfig, lastTier, redisClient, ms, message) => {
+	console.log("mute");
+	embedTemp.setTitle("You Have Been Tiered!");
+	embedTemp.setColor("#ff0000");
+	embedTemp.addFields({ name: `Tier:`, value: `${tierArg}`, inline: false });
+	embedTemp.addFields({ name: `Type:`, value: `${await tierAction.matchPunishment(tier, dbResConfig, lastTier)}`, inline: false });
+	embedTemp.addFields({ name: `Time:`, value: `How much Time?: ${ms(await tierAction.matchTier(tier, dbResConfig, lastTier))}`, inline: false });
+	embedTemp.addFields({ name: `Reason:`, value: `${reason}`, inline: false });
+	tagged.send(embedTemp);
+
+	if(!tagged.roles.cache.has(dbResConfig.mutedRole)) {
+		try {
+			await tagged.roles.set([]).then(()=> {
+			tagged.roles.add(dbResConfig.mutedRole);
+			});
+		} catch(err) {
+				console.log("Cant Remove this Boi");
+			}
+		}
+		
+		
+	const redisKey = `muted-${tagged.id}-${message.guild.id}-${tierArg}`
+	redisClient.set(redisKey, 'true', 'EX', (await tierAction.matchTier(tier, dbResConfig, lastTier) / 1000));
+	message.channel.send(`Sucessfully muted <@${tagged.id}> for T${lastTier + 1}`);
+}
+
+
+const awaitBan = async(tierAction, embedTemp, tier, tagged, reason, tierArg, dbResConfig, lastTier, redisClient, ms, message) => {
+	console.log("ban");
+	embedTemp.setTitle("You Have Been Tiered!");
+	embedTemp.setColor("#ff0000");
+	embedTemp.addFields({ name: `Tier:`, value: `${tierArg}`, inline: false });
+	embedTemp.addFields({ name: `Type:`, value: `${tierAction.matchPunishment(tier, dbResConfig, lastTier)}`, inline: false });
+	embedTemp.addFields({ name: `Time:`, value: `${ms(await tierAction.matchTier(tier, dbResConfig, lastTier))}`, inline: false });
+	embedTemp.addFields({ name: `Reason:`, value: `${reason}`, inline: false });
+	tagged.send(embedTemp);
+
+	try {
+		const redisKey = `banned-${tagged.id}-${message.guild.id}`
+
+		redisClient.set(redisKey, 'true', 'EX', (await tierAction.matchTier(tier, dbResConfig, lastTier) / 1000));
+	} finally {
+	}
+
+	await tagged.ban(reason).catch(err => {
+		message.channel.send("Cant Kick User: Higher Permissions");
+		throw err;
+	})
+	message.channel.send(`Sucessfully banned <@${tagged.id}> for T${lastTier + 1}`);
+}
 
 module.exports.help = {
 	name: "Tier User",
---
+++ utils/queries/tierUtils/tierUtils.js
@@ -0,0 +1,66 @@
+const serverStats = require("../../schemas/serverstat.js");
+
+
+//Function to globally query the User in that server, and pass the information down to every command
+
+
+
+const addTier = async(client, message, user, tier, userRoles, tierArg, lastTier, tagged) => {
+	if(tier) {
+		console.log("Adding to set");
+		serverStats.updateOne({guildId: message.guild.id, "guildMembers.userID": tagged.id} , {
+			$addToSet:{
+			"guildMembers.$.punishmentsTiers": {
+				tierName: tier.serverTiers[0].TierName,
+				tierLevel: 1,
+				TierForgiveness: tier.serverTiers[0]*(lastTier + 1),
+				OffenderMsgCount: user.guildMembers[0].messageCount,
+				tierTime: tier.serverTiers[0].TierTimes * (lastTier + 1),
+				pastRoles: userRoles
+			}
+		}}, {upsert: true}).exec();
+	} else {
+		console.log("Updating set");
+		serverStats.updateOne({
+			guildId: message.guild.id, 
+			"guildMembers.userID": tagged.id,
+		}, 
+		{
+			$set:{
+				"guildMembers.$.punishmentsTiers.$[punishmentName].pastRoles": {
+					userRoles,
+				},
+			},
+			"guildMembers.$.punishmentsTiers.$[punishmentName].tierTime": tier.serverTiers[0].TierTimes * (lastTier + 1),
+			$inc:{
+				"guildMembers.$.punishmentsTiers.$[punishmentName].tierLevel":1
+			},
+			},
+			{ "arrayFilters": [
+				{ "punishmentName.tierName": tier.serverTiers[0].TierName }
+			] }).exec();
+		}
+
+}
+
+const matchTier = async(tier, dbResConfig, lastTier) => {
+	if(parseInt(lastTier) >= (parseInt(tier.serverTiers[0].TierTimes.length) - 1)) {
+		return (tier.serverTiers[0].TierTimes[parseInt(tier.serverTiers[0].TierTimes.length) -1])
+	} else {
+		return tier.serverTiers[0].TierTimes[lastTier]
+	}
+};
+
+	//Gets the Type of Punishment
+const matchPunishment = async(tier, dbResConfig, lastTier) => {
+	if(parseInt(lastTier) >= (parseInt(tier.serverTiers[0].banOrMute.length) - 1)) {
+		return (tier.serverTiers[0].banOrMute[parseInt(tier.serverTiers[0].banOrMute.length) -1])
+	} else {
+		return tier.serverTiers[0].banOrMute[lastTier]
+	}
+};
+
+
+module.exports = {addTier, matchTier, matchPunishment}
+
+//Hi
