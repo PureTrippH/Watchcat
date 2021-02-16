811f956d636c4257ff83ddc8a58ef5db4b105405
Tripp H <capehenrykid@gmail.com>
2021-02-16 11:16:18 -0500

    Welcome Image Maker

--- Bot/commands/welcome.js
+++ Bot/commands/welcome.js
@@ -5,52 +5,21 @@ exports.run = async (client, message, args) => {
   const mongoose = require('mongoose');
   const embed = new Discord.MessageEmbed();
   const welcomeInfo = {
-    joinImg,
-    leaveImg,
-    welcomeChannel,
+    joinImg: null,
+    leaveImg: null,
+    welcomeChannel: null,
   }
-
-  if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.author.send("You Cant Use Embeds");
+    
   embed.setTitle("Edit Welcome Images");
-  embed.setDescription("Welcome to the New Embed Builder. Choose An Option Below.");
+  embed.setDescription("Welcome to the Welcome Image Creator. Please React with the Proper Emoji to Use Me!");
   embed.addFields({ name: `1️⃣`, value: `Set Join Image`});
   embed.addFields({ name: `2️⃣`, value: `Set Leave Image`});
-  message.channel.send(embed).then(msg => {
-		msg.react('1️⃣');
-		msg.react('2️⃣');
-		msg.react('💾');
-  msg.awaitReactions((reaction, user) => user.id == message.author.id, { max: 1 }).then(async collected => {
-			let reaction = collected.first().emoji.name;
-			console.log(reaction);
-			switch(reaction) {
-        case '1️⃣':
-          message.channel.send("Please Copy Paste an Image Link For the **JOIN IMAGE**");
-          welcomeInfo.joinImg = await collectMsg(message);
-          return this.run(client, message, args);
-				break;
-        case '2️⃣':
-          message.channel.send("Please Copy Paste an Image Link For the **LEAVE IMAGE**");
-          welcomeInfo.leaveImg = await collectMsg(message);
-          return this.run(client, message, args);
-				break;
-        case '3️⃣':
-          message.channel.send("Please Copy the **Channel ID** for the Welcome Channel");
-          welcomeInfo.welcomeChannel = await collectMsg(message);
-          return this.run(client, message, args);
-				break;
-        case '💾':
-          serverConf.findOneAndUpdate({
-            guildId: message.guild.id
-          },{
-            welcomeInfo
-          }).then(err => {
-            message.channel.send("Successfully set Welcome Image");
-          });
-        break;
+  embed.addFields({ name: `3️⃣`, value: `Set Welcome Channel`});
+  embed.addFields({ name: `💾`, value: `Save Progress`});
 
-      }
-      });
-  });
+  if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.author.send("You Cant Use Embeds");
+  await reactCreator(embed, welcomeInfo, serverConf, client, message, args);
+
 }
 
 const collectMsg = async(message) => {
@@ -60,6 +29,46 @@ const collectMsg = async(message) => {
 		return msg.first().content;
 }
 
+const reactCreator = async(embed, welcomeInfo, serverConf, client, message, args) => {
+  message.channel.send(embed).then(msg => {
+		msg.react('1️⃣');
+		msg.react('2️⃣');
+    msg.react('3️⃣');
+		msg.react('💾');
+  msg.awaitReactions((reaction, user) => user.id == message.author.id, { max: 1 }).then(async collected => {
+			let reaction = collected.first().emoji.name;
+			console.log(reaction);
+			switch(reaction) {
+        case '1️⃣':
+          message.channel.send("Please Copy Paste an Image Link For the **JOIN IMAGE**");
+          welcomeInfo.joinImg = await collectMsg(message);
+          return this.welcomeMaker(embed, welcomeInfo, serverConf, client, message, args);
+				break;
+        case '2️⃣':
+          message.channel.send("Please Copy Paste an Image Link For the **LEAVE IMAGE**");
+          welcomeInfo.leaveImg = await collectMsg(message);
+          return this.welcomeMaker(embed, welcomeInfo, serverConf, client, message, args);
+				break;
+        case '3️⃣':
+          message.channel.send("Please Copy the **Channel ID** for the Welcome Channel");
+          welcomeInfo.welcomeChannel = await collectMsg(message);
+          return this.welcomeMaker(embed, welcomeInfo, serverConf, client, message, args);
+				break;
+        case '💾':
+          serverConf.findOneAndUpdate({
+            guildId: message.guild.id
+          },{
+            welcomeInfo
+          }).then(err => {
+            message.channel.send("Successfully set Welcome Image Info");
+          });
+        break;
+
+      }
+      });
+  });
+}
+exports.welcomeMaker = reactCreator;
 
 module.exports.help = {
   name: "Welcome Editor",
